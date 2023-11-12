554bfbdc87be518facce41bd4afaf7ac90fa4bee
angelo54425 <e.gasana@alustudent.com>
2023-11-12 20:13:59 +0000

    2. Recurse it!

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,52 +1,23 @@
 #!/usr/bin/python3
-"""
-recursive function that
-returns a list containing
-titles of all hot articles
-"""
+"""fetches the title of all hot posts for a given subreddit recursively"""
 
-
-import json
 import requests
-import sys
 
 
-def recurse(subreddit, hot_list=None, after=None):
-    """
-    recursive function
-    """
-    if hot_list is None:
-        hot_list = []
-
+def recurse(subreddit, hot_list=[], after=""):
+    """Main function"""
     URL = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {
-            "User-Agent": "2-recurse/1.0"
-    }
-    params = {"after": after} if after else{}
-    response = requests.get(URL, headers=headers, params=params)
 
-    if (response.status_code) == 200:
-        data = response.json().get("data", {})
-        children = data.get("children", [])
-
-        for child in children:
-            title = child.get("data", {}).get("title")
-            if title:
-                hot_list.append(title)
-
-                after = data.get("after")
-
-                if after:
-                    recurse(subreddit, hot_list, after)
-
-                else:
-                    return hot_list
-
-            else:
-                return ('None')
-
-    else:
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    PARAMS = {"after": after, "limit": 100}
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, params=PARAMS,
+                                allow_redirects=False)
+        after = RESPONSE.json().get("data").get("after")
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [hot_list.append(post.get('data').get('title')) for post in HOT_POSTS]
+        if after is not None:
+            return recurse(subreddit, hot_list, after)
+        return hot_list
+    except Exception:
         return None
-
-if __name__ == "__main__":
-    pass
